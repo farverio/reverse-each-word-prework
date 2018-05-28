7528c2423e05b571ca4f5bc198ddc8b1a968382a
Stephen F <stephen@farver.io>
2018-05-28 01:43:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,11 @@
-def reverse_each_word(string)
-  word_arr = string.split("")
+def reverse_each_word(sentence)
+  word_arr = sentence.split("")
+  word_arr.map {|word| reverse_word(word)}
+end
+
+def reverse_word(word)
+  word_arr = word.split("")
   output = ""
   word_arr.each {|ch| output.unshift(ch)}
-  output.join("")
 end
   
